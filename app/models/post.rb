e5bce02ff34874021f5fe63ccd3cb0edc87931fc
Steve Klabnik <steve@steveklabnik.com>
2014-06-10 16:26:30 -0400

    Add blog index page, and enhance Post for it.
    We need to make Post pretend like ActiveModel to be able to do nice things
    with it.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post
-  attr_accessor :title, :body, :created_at
+  extend ActiveModel::Naming
+
+  attr_accessor :title, :body, :slug, :created_at
 
   def self.find(slug)
     path = File.join(Rails.root, "lib/posts/#{slug}.md")
@@ -9,8 +11,25 @@ class Post
 
     new.tap do |post|
       post.title = rendered.metadata["title"]
+      post.slug = rendered.metadata["slug"]
       post.body = rendered.output.html_safe
       post.created_at = File.ctime(path)
     end
+  end
+
+  def self.all
+    path = File.join(Rails.root, "lib/posts/")
+
+    entries = Dir.entries(path)
+    entries.delete(".")
+    entries.delete("..")
+
+    entries.collect do |filename|
+      find(filename[0..-4]) # chop off ".md"
+    end
+  end
+
+  def to_param
+    slug
   end
 end
