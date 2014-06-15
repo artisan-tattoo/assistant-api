4cc4482517c3b9a47e53d6e68fbf3b2b6cafa5d6
Steve Klabnik <steve@steveklabnik.com>
2014-06-15 01:21:14 -0400

    Add a sign out link to the home page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get  "/sign_in", to: "sessions#new"
   post "/sign_in", to: "sessions#create"
+  delete "/sign_out", to: "sessions#destroy"
 
   resources :posts, only: [:index, :show]
 end
