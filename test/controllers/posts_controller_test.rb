fd265dc7da923c0e81f21eedb9f2c297f8025caa
Andrew Sardone <andrew@andrewsardone.com>
2014-06-17 09:35:41 -0400

    Simplify RSS <link> test w/ assert_select
    No need to mess with Nokogiri -- we can just use [assert_select][as].
    
    [as]: http://cl.ly/2Q0Z2s3h3M2s

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,6 @@
 class PostsControllerTest < ActionController::TestCase
   test "should include an RSS <link>" do
     get :index
-    doc = Nokogiri::HTML(@response.body)
-    assert doc.xpath("//link[@type='application/rss+xml']").count > 0,
-          "Posts index page should include auto-detectable RSS <link>"
+    assert_select "link[type='application/rss+xml']"
   end
 end
