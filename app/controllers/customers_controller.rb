758b5870ca42d0b30e7b2a1f9cf8f9f3e5a66af9
Steve Klabnik <steve@steveklabnik.com>
2014-06-17 22:32:52 -0400

    You may not try to edit another shop's customers.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,6 +13,11 @@ class CustomersController < ApplicationController
 
   def edit
     @customer = Customer.find(params[:id])
+    if @customer.shop_id != current_shop.id
+      flash[:error] = "Sorry, you can't do that."
+
+      redirect_to dashboard_path
+    end
   end
 
   def update
---
+++ test/features/authorization_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class AuthorizationTest < Capybara::Rails::TestCase
+  test "A shop can only edit its own customers" do
+    shop = sign_in
+    shop2 = Shop.create(
+      name: "Innocent Shop",
+      email: "innocent@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    customer = shop2.customers.create
+
+    visit edit_customer_path(customer)
+
+    page.must_have_content "Sorry, you can't do that."
+
+    page.current_path.must_equal dashboard_path
+  end
+end 
