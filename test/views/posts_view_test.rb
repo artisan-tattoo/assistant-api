8deb3220964ffe7499975100834c9d1e9d3e7340
Ashley Williams <ashley@bocoup.com>
2015-05-02 15:53:56 -0400

    remove tests that rely on views

--- test/views/posts_view_test.rb
+++
@@ -1,9 +0,0 @@
-class PostsViewTest < ActionView::TestCase
-  test "should include an RSS <link>" do
-    @posts = []
-    render template: "posts/index.html.erb",
-           layout: 'layouts/application.html.erb',
-           locals: { current_shop: nil }
-    assert_select "link[type='application/rss+xml']"
-  end
-end
