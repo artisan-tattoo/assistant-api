fc0ce31ba8d40bae356219179378f1b70078dac0
ag_dubs <ashley666ashley@gmail.com>
2015-08-08 15:29:56 -0400

    add jwt auth strategy

--- src/index.js
+++ src/index.js
@@ -7,54 +7,66 @@ const cors = require('cors');
 
 const modulePath = path.join(__dirname, 'modules');
 const resources = fs.readdirSync(modulePath);
+const Store = require('./modules/stores/model');
 
 const app = express();
 
 const API = require('./classes/api');
-const Authentication = require('./classes/authentication');
 
-const passport = require('passport');
-const BasicStrategy = require('passport-http').BasicStrategy;
-
-var Store = require('./modules/stores/model');
-
-passport.use(new BasicStrategy({
-  },
-  function(email, password, done) {
-    Authentication.find(Store, email, password, function(model) {
-      if (!model) {
-        return done(null, false, { message: 'Wrong username or password' });
-      } else {
-        return done(null, model);
-      }
-    });
-  }
-));
-
-app.use(passport.initialize());
+const config = require('./config.json');
+const jwt = require('express-jwt');
+const jwtCheck = jwt({
+  secret: config.secret
+});
 
 app.use(cors());
-
-app.use(passport.authenticate('basic', { session: false }));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({
   type: ['application/json', 'application/vnd.api+json']
 }));
-
-resources.forEach(function (resource) {
-  API.register(resource);
-  app.use(API.endpoint(resource));
-});
 
 app.get('/v1', function (request, response) {
   response.set('Content-Type', 'application/json');
   response.send(JSON.stringify(API.index(), null, 2));
 });
 
+const jwtoken = require('jsonwebtoken');
+
+var createToken = function(store) {
+  var store_nopassword = delete store.password;
+  return jwtoken.sign(store_nopassword, config.secret, { expiresInMinutes: 60*5 });
+};
+
+app.post('/sessions/create', function(req, res) {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send("You must send the email and the password");
+  }
+
+  var store = Store.where({
+    email: req.body.email
+  }).fetch()
+  .then(function(model) {
+    return model;
+  });
+
+  if (!store || (store.password !== req.body.password) ) {
+    return res.status(401).send("The email and password do not match.");
+  }
+
+  res.status(201).send({
+    id_token: createToken(store)
+  });
+});
+
 app.get('/', function (request, response) {
   response.redirect('/v1');
 });
 
+app.use(jwtCheck);
+
+resources.forEach(function (resource) {
+  API.register(resource);
+  app.use(API.endpoint(resource));
+});
 
 module.exports = app;
