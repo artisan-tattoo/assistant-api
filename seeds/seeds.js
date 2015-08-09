cd2ef564a701e4a9f03b9e96658c87729a80b18f
ag_dubs <ashley666ashley@gmail.com>
2015-08-08 23:59:24 -0400

    seed data for dateonwaitinglist on customer needs to be a string

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -27,7 +27,7 @@ exports.seed = function(knex, Promise) {
       name: "Steve",
       phone: "412 555 5555",
       email: "steve@example.com",
-      dateonwaitinglist: new Date(),
+      dateonwaitinglist: (new Date()).toString(),
       status: 1,
       local: false,
       location: "Brooklyn",
